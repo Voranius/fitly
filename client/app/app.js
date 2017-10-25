0648370c95031c303e0a24c3fe3e53d3c2edb366
Cheen Chong <cheen@genii-group.com>
2017-10-25 11:52:16 +0800

    validation to login & reg pages

--- client/app/app.js
+++ client/app/app.js
@@ -850,7 +850,7 @@
         var clientRegCtrl = this;
 
         clientRegCtrl.user = {};
-        clientRegCtrl.message = ""; 
+        clientRegCtrl.message = "";
         clientRegCtrl.user.role = 2;
         clientRegCtrl.user.status = "";
 
